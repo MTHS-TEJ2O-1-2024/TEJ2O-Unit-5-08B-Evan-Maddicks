8837935d01f82e551eb11d398299326f39f2df35
Evan-Maddicks <evan.maddicks@mths.ca>
2024-11-07 16:32:23 -0500

    This program will essentially act like a roomba

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Nov 2024
+ * This program will essentially act like a roomba
 */
 
-basic.showString('Hello, World!')
+//variables
+let distanceToObject = PingUnit.Centimeters
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+// loop
+input.onButtonPressed(Button.A, function () { 
+    distanceToObject = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P0,
+    PingUnit.Centimeters)
+    if (distanceToObject > 10) {
+        //move car forward
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(5,48)
+        basic.pause(250)
+        sonar.ping
+    } else {
+        //move car back and turn
+        robotbit.StpCarMove(-10, 48)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)}
+    })
